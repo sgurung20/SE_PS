573af2eefd6f22a04731b89c75cb05d6ff9f556a
sgurung20 <74170066+sgurung20@users.noreply.github.com>
2021-03-12 14:17:54 -0500

    Add files via upload

---
+++ HOMEWORK/finalProj/note_app/src/AuthenticationService.js
@@ -0,0 +1,25 @@
+class AuthenticationService {
+    registerSuccessfulLogin(username, password){
+        console.log('registerSuccessfulLogin')
+        sessionStorage.setItem('authenticatedUser', username);
+    }
+logout(){
+    sessionStorage.removeItem('authenticatedUser')
+}
+
+isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    if(user===null) return false
+    return true
+}
+
+getLoggedInUsername(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    console.log(user)
+    if(user===null) return ''
+    return user
+}
+
+}
+
+export default new AuthenticationService()
---
+++ HOMEWORK/finalProj/note_app/src/LogoutComponent.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class LogoutComponent extends Component{
+    render(){
+        return (
+        <> 
+             <h1>Logged out!</h1>
+             <div className="container">
+                 Thank you for using my Notes application!
+             </div>
+        </>
+        )
+    }
+}
+
+
+export default LogoutComponent
