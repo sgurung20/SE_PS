95fa1bb8372a940db317ab8cc5eef8bf7a29a752
sgurung20 <74170066+sgurung20@users.noreply.github.com>
2021-03-12 13:49:50 -0500

    Add files via upload

---
+++ src/mod3week1hw3/Cuboid.java
@@ -0,0 +1,18 @@
+package mod3week1hw3;
+
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
